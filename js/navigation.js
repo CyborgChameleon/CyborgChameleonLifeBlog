193a4009a4a53429ae635db1928930e409b4dfae
CyborgChameleon <diabloftdih@gmail.com>
2022-05-06 15:30:25 -0400

    updates and clean up

---
+++ js/navigation.js
@@ -0,0 +1,99 @@
+var headerNavigationObject = {headerNavigationArray:[
+    {
+        title : "CYBORG CHAMELEON",
+        location:"index.html",
+        dropdown:[
+            {
+                title : "More Coming Soon",
+                location:""
+            }
+        ]
+    },
+    {
+        title : "Schedule",
+        location:"schedule/schedule.html"
+    },/*
+    {
+        title : "More Coming Soon",
+        location:""
+    },
+    {
+        title : "More Coming Soon",
+        location:""
+    },
+    {
+        title : "More Coming Soon",
+        location:""
+    },*/
+    {
+        title : "More Coming Soon",
+        location:"",
+        dropdown:[
+            {
+                title : "More Coming Soon",
+                location:""
+            }
+        ]
+    }
+]};
+
+
+var sideNavigationObject = {sideNavigationArray:[
+    {
+        title : "Schedule",
+        location:"schedule/schedule.html"
+    },
+    {
+        title : "More Coming Soon",
+        location:""
+    }
+]};
+
+var myOtherWebsitesObject = {myOtherWebsitesArray:[
+    {
+        title : "Coming Soon",
+        location:""
+    },
+    {
+        title : "Coming Soon",
+        location:""
+    }
+]};
+
+/* 
+Figure out how to seperate the array to a seperated json file and import it 
+
+https://youtu.be/mw5i_QGDomw
+https://www.tutorialspoint.com/how-to-import-local-json-file-data-to-my-javascript-variable
+https://dmitripavlutin.com/fetch-with-json/
+https://jsononline.net/json-validator 
+*/
+
+
+document.getElementById("headerNavigationDirectory").innerHTML=headerNavigationObject.headerNavigationArray
+.map(
+    (page) =>
+    `<li><a href="${page.location}">${page.title}</a></li>`
+)
+.join("");
+document.getElementById("sideNavigationDirectory").innerHTML=sideNavigationObject.sideNavigationArray
+.map(
+    (page) =>
+    `<li><a href="${page.location}">${page.title}</a></li>`
+)
+.join("");
+
+
+document.getElementById("footer").innerHTML=
+`<div>`+
+    `<h4>My Other Websites</h4>`+
+    
+    myOtherWebsitesObject.myOtherWebsitesArray
+    .map(
+        (page) =>
+        `<a href="${page.location}">${page.title}<br></a>`
+    )
+    .join("")
++
+`</div>`
+;
